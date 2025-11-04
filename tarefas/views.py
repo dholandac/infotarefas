f5afbc1785c0c8f6a9f562c2770924d42684d761
dholandac <daniel.holanda.campos@gmail.com>
2025-11-04 17:28:05 -0300

    ajustes

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import Task
 
 def home(request):
-    return render(request, 'tarefas/home.html')
+    tasks = Task.objects.all()
+    completed_tasks = len(tasks)
+    total_tasks = 0
+    remaining_tasks = 0
+    pct = 0
 
+    return render(
+        request, 
+        'tarefas/home.html',
+        {
+            'tasks' : tasks,
+            'total' : total_tasks,
+            'completed' : completed_tasks,
+            'remaining' : remaining_tasks,
+            'pct' : pct
+        })
