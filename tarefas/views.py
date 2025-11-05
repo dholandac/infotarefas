1f14c5e57f8d792fa3232b0acd4c6ab693c03b45
dholandac <daniel.holanda.campos@gmail.com>
2025-11-05 15:30:48 -0300

    comecando logica de adicao de tarefa

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 def home(request):
     tasks = Task.objects.all()
@@ -18,3 +19,10 @@ def home(request):
             'remaining' : remaining_tasks,
             'pct' : pct
         })
+
+def add(request):
+    form = TaskForm(request.POST)
+    if form.is_valid():
+        form.save()
+        
+    return redirect('home')
