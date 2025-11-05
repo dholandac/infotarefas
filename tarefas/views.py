8b7d848be208d402d75e422b6b86f5730eefcad9
dholandac <daniel.holanda.campos@gmail.com>
2025-11-05 17:09:20 -0300

    logica da checklist e delete

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 from .forms import TaskForm
 
 def home(request):
     tasks = Task.objects.all()
-    completed_tasks = len(tasks)
-    total_tasks = 0
+    completed_tasks = 0
+    total_tasks = len(tasks)
     remaining_tasks = 0
     pct = 0
+
+    for task in tasks:
+        if task.done == True:
+            completed_tasks += 1
+
+    remaining_tasks = total_tasks - completed_tasks
+
+    if total_tasks > 0:
+        pct = int((completed_tasks / total_tasks) * 100)
 
     return render(
         request, 
@@ -24,5 +33,23 @@ def add(request):
     form = TaskForm(request.POST)
     if form.is_valid():
         form.save()
-        
+
+    return redirect('home')
+
+def toggle(request, id):
+    task = get_object_or_404(Task, id=id)
+
+    if task.done == True:
+        task.done = False
+    else:
+        task.done = True
+
+    task.save()
+
+    return redirect('home')
+
+def delete(request, id):
+    task = get_object_or_404(Task, id=id)
+
+    task.delete()
     return redirect('home')
